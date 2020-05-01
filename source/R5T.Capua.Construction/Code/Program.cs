6fad80be38f2801f23724f6b269ee66b6c59100c
MinexAutomation <minex.automation@gmail.com>
2020-05-01 10:53:23 -0700

    Added basic async-Main, program-as-a-service functionality.

--- source/R5T.Capua.Construction/Code/Program.cs
+++ source/R5T.Capua.Construction/Code/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Hosting;
+
+using R5T.Liverpool;
+
 
 namespace R5T.Capua.Construction
 {
-    class Program
+    class Program : HostedServiceProgramBase // AsyncHostedServiceProgramBase
     {
+        public Program(IApplicationLifetime applicationLifetime)
+            : base(applicationLifetime)
+        {
+        }
+
+        //static async Task Main(string[] args)
+        //{
+        //    await HostedServiceProgram.RunAsync<Program>(args);
+        //}
+
+        //protected override Task SubMain()
+        //{
+        //    Console.WriteLine("Hello world!");
+
+        //    throw new NotImplementedException();
+
+        //    //return Task.CompletedTask;
+        //}
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HostedServiceProgram.Run<Program>(args);
+        }
+
+        protected override void SubMain()
+        {
+            Console.WriteLine("Hello world!");
+
+            throw new NotImplementedException();
+
+            //return Task.CompletedTask;
         }
     }
 }
---
+++ source/R5T.Capua.Construction/Code/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Microsoft.Extensions.Logging;
+
+using R5T.Richmond;
+
+
+namespace R5T.Capua.Construction
+{
+    class Startup : StartupBase
+    {
+        public Startup(ILogger<StartupBase> logger)
+            : base(logger)
+        {
+        }
+    }
+}
